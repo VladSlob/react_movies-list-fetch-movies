978eeb73f0acecfcaabac8e5968015ba7dc01620
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-20 13:26:30 +0300

    api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ResponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=8efff68';
+const API_URL = 'https://www.omdbapi.com/?apikey=e3280c07';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  return fetch(`${API_URL}&t=${encodeURIComponent(query)}`)
+  return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())
     .catch(() => ({
       Response: 'False',
